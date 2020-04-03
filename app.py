7b623fc05d9a61c89e045634dbde577ff87c3b00
Duvan Marriaga Villar <fernandamarciales9830@gmail.com>
2020-04-03 12:53:56 -0500

    1er commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+from flask import request, redirect,url_for
+import re 
+
+app =Flask(__name__)
+
+@app.route('/')
+def Index():
+    return render_template('index.html')
+
+@app.route('/results')
+def rs():
+    return render_template('resps.html')
+
+@app.route('/resultn')
+def rn():
+    return render_template('respn.html')
+
+@app.route('/comprobar',methods=['POST'])
+def validar():
+    if request.method == 'POST':
+        cadena=request.form['expresion']
+        val=validate(cadena)
+        if val:
+            return redirect(url_for('rs'))
+        else:
+            return redirect(url_for('rn'))
+    return "no recibido"
+
+
+def validate(cad):
+    lenguaje = re.match('(?=\w*[A-Z])(?=\w*\d){3}(?=\w*[a-z]){3}',cad) 
+    if lenguaje is None:
+        return False
+    else:
+        return True
+
+if __name__ == '__main__' :
+    app.run(port=3000,debug=True)
